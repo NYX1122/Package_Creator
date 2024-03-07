1b142392068775178117468c0f0fdd85e6b11650
BoDee Angus <bodee.angus@gmail.com>
2024-03-06 18:30:32 -0700

    Finished Package

---
+++ app.js
@@ -0,0 +1,37 @@
+import terminalAsker from 'terminal_asker';
+
+import siblingFolderReader from 'sibling_folder_reader';
+
+import githubManager from 'github_manager';
+
+import repositoryCloner from 'repository_cloner';
+
+import packageConfigurer from 'package_configurer';
+
+export default async function () {
+  try {
+    const packageName = await terminalAsker('packageName');
+    const packageDesc = await terminalAsker('packageDesc');
+
+    console.log(packageName);
+
+    const siblingFolders = await siblingFolderReader();
+
+    if (siblingFolders.includes(packageName)) {
+      throw new Error(
+        'Package already exists with the specified package name. Please choose a different package name.'
+      );
+    } else {
+      const { html_url } = await githubManager('create', [
+        packageName,
+        packageDesc,
+      ]);
+
+      await repositoryCloner(html_url, packageName);
+      await packageConfigurer(packageName, packageDesc, html_url);
+    }
+  } catch (error) {
+    console.error('Package_Creator encountered an error:');
+    console.error(error);
+  }
+}
